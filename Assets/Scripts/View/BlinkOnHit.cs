007713dde8c103d276fb25ddf17e1430f13900a1
Nadav Litver <Nlitver@gmail.com>
2023-06-18 15:02:27 +0300

    using unitask to replace coroutines trying to make less classes mono

--- Assets/Scripts/View/BlinkOnHit.cs
+++ Assets/Scripts/View/BlinkOnHit.cs
@@ -1,31 +1,32 @@
-using System.Collections;
 using UnityEngine;
 using Zenject;
+using Cysharp.Threading.Tasks;
 
 namespace view
 {
-    public class BlinkOnHit : MonoBehaviour,IBlinkOnHit// created only to make a small blinking effect when player is damaged
+    public class BlinkOnHit :IBlinkOnHit// created only to make a small blinking effect when player is damaged
     {
         [Inject] private SpriteRenderer robotSR;
         private Color blinkColor = new Color(1, 1, 1, 0.25f);
         private Color startingColor = Color.white;
-      
+
 
         public void CallBlinkRoutine(int currentHealth)
         {
             if (currentHealth >= 0)
             {
-                StartCoroutine(BlinkRoutine());
+               _= BlinkRoutine();
             }
         }
-        IEnumerator BlinkRoutine()
+
+        private async UniTaskVoid BlinkRoutine()
         {
             robotSR.color = blinkColor;
-            yield return new WaitForSeconds(0.25f);
+            await UniTask.Delay(250);
             robotSR.color = startingColor;
-            yield return new WaitForSeconds(0.25f);
+            await UniTask.Delay(250);
             robotSR.color = blinkColor;
-            yield return new WaitForSeconds(0.25f);
+            await UniTask.Delay(250);
             robotSR.color = startingColor;
         }
     }
